9c10e00fa40982dfc679468e9d8a2b6caeb7029c
Priya Pradhan <priy437pradhan@gmail.com>
2024-09-01 19:22:31 +0530

    my first commit

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -33,7 +33,7 @@ const PhotoGallery = () => {
             <img
               src={photo}
               alt={`Gallery ${index}`}
-              className="w-full h-40 object-cover rounded-lg"
+              className="w-full h-auto object-cover rounded-lg"
             />
           </motion.div>
         ))}
